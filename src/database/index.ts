96f9efe1dbf3d7d0e6e0e48a895538421b4c89eb
JeanCDev <jeangames15@gmail.com>
2021-05-20 20:49:08 -0300

    Add ssl

--- src/database/index.ts
+++ src/database/index.ts
@@ -6,7 +6,8 @@ export const connection = new Client({
   database: process.env.DB_DATABASE,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT)
+  port: Number(process.env.DB_PORT),
+  ssl: true
 });
 
 connection.connect();
